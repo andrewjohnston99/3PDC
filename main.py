4228a185cb7d4a5a27b0a38ce8ccb2aea0218e15
Clay Wilson <42412332+Lwilson67gatech@users.noreply.github.com>
2021-03-10 11:10:47 -0500

    Added the basic script, having full version control issues with pycharm

---
+++ main.py
@@ -0,0 +1,28 @@
+import requests
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    urls = [
+        "http://navigator-c2c.dot.ga.gov/snapshots/ATL-CAM-908.jpg",
+        "http://navigator-c2c.dot.ga.gov/snapshots/ATL-CAM-939.jpg",
+        "http://navigator-c2c.dot.ga.gov/snapshots/ATL-CAM-085.jpg",
+        "http://navigator-c2c.dot.ga.gov/snapshots/ATL-CAM-974.jpg",
+        "http://navigator-c2c.dot.ga.gov/snapshots/GDOT-CAM-017.jpg"
+    ]
+    index = 0;
+    for url in urls:
+        r = requests.get(url)
+        filename = "ATL-CAM-" + str(index) + ".jpg"
+        index += 1
+        with open(filename, "wb") as f:
+            f.write(r.content)
