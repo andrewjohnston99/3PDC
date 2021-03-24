d749b94c59df83a037f925aef3b1831bc3c27b1c
Clay Wilson <lwilson67@mail.gatech.edu>
2021-03-23 23:17:04 -0400

    Video from stream is now saved in a file

--- ATL Traffic - Python Scripts/OpenCVTEst.py
+++ ATL Traffic - Python Scripts/OpenCVTEst.py
@@ -1,9 +1,19 @@
 import numpy as np
 import cv2
+import os
+from datetime import date
 # Open a sample video available in sample-videos
 vcap = cv2.VideoCapture('rtsp://vss2live.dot.ga.gov:80/lo/gdot-cam-017.stream')
 if not vcap.isOpened():
     print("File Cannot be Opened")
+
+frame_width = int(vcap.get(3))
+frame_height = int(vcap.get(4))
+size = (frame_width, frame_height)
+
+filename = date.today().strftime("%B %d, %Y") + ".avi"
+
+video = cv2.VideoWriter(filename, cv2.VideoWriter_fourcc(*'MJPG'), 10, size)
 
 while(True):
     # Capture frame-by-frame
@@ -12,6 +22,7 @@ while(True):
     if frame is not None:
         # Display the resulting frame
         cv2.imshow('frame',frame)
+        video.write(frame)
         # Press q to close the video windows before it ends if you want
         if cv2.waitKey(22) & 0xFF == ord('q'):
             break
@@ -21,5 +32,6 @@ while(True):
 
 # When everything done, release the capture
 vcap.release()
+video.release()
 cv2.destroyAllWindows()
 print("Video stop")
