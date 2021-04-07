f72a8f37cc43e4cccffc7829d85638c04a538c05
Andrew Johnston <42980380+andrewjohnston99@users.noreply.github.com>
2021-04-07 10:25:44 -0400

    Update config.py

--- Scheduler/config.py
+++ Scheduler/config.py
@@ -2,3 +2,5 @@
 
 cookie_file = "/Scheduler/google.com_cookies.txt"
 rpi_google_email = "YOUR_EMAIL_HERE"
+person_full_name = "YOUR FULL NAME HERE (From Google Account that will share)"
+cfb_polling_interval_secs = 180 # Default 3 min
