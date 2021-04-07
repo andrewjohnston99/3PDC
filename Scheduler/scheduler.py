0673feee279ab75bb7ddc9fbdaad220076fbc0b5
Andrew Johnston <42980380+andrewjohnston99@users.noreply.github.com>
2021-04-07 19:30:25 -0400

    Add session init tasks

--- Scheduler/scheduler.py
+++ Scheduler/scheduler.py
@@ -1,6 +1,7 @@
 import logging
 import threading
 import time
+import datetime
 import config
 from locationsharinglib import Service
 
@@ -11,29 +12,39 @@ cfb_polling_interval = config.cfb_polling_interval_secs
 next_cleanup_time = [""]
 
 # [Broadcast Active, Session Initiated, Session Active, ID, Directory], Global & Mutable for Threads
-session_status = [False, False, False, -1, "/"]
+session_status = [False, False, False, "", "/"]
 cfb_thread_started = False
 cleanup_thread_started = False
 
 # Check to see if location sharing has started. Check every polling interval secs.
 def check_for_broadcast(session_status):
     while(not session_status[0]):
+        time.sleep(cfb_polling_interval)
         print("Checked!")
         service = Service(cookies_file=cookies_file, authenticating_account=google_email)
         for person in service.get_all_people():
             if (person._full_name == person_full_name):
                 print("Updated session status!")
                 session_status[0] = True
-        time.sleep(cfb_polling_interval)
 
 # Create folders and setup anything else necessary for a collection session.
 def initialize_session(session_status):
     print("Now initializing!")
     # TODO Create Folders...
+
     # Sleep to simulate loading...
     time.sleep(10)
+
+    name = (person_full_name.replace(" ","") + "_")
+    init_date = datetime.datetime.now()
+    session_ID = name + init_date.strftime("%Y-%m-%d_%H:%M")
+
+    print(session_ID)
+
+    session_status[3] = session_ID
     session_status[1] = True
 
+# Poll location more frequently and handle invoking camera recordings
 def session_handler(session_status):
     # TODO
     return
