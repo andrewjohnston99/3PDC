8a6646280d0c7d2357c3867fcf9bd88fcc481d37
Andrew Johnston <42980380+andrewjohnston99@users.noreply.github.com>
2021-04-08 13:26:40 -0400

    Update config.py

--- Scheduler/config.py
+++ Scheduler/config.py
@@ -4,3 +4,4 @@ cookie_file = "google.com_cookies.txt"
 rpi_google_email = "YOUR_PI_EMAIL_HERE"
 person_full_name = "YOUR FULL NAME HERE (From Google Account that will initiate the share)"
 cfb_polling_interval_secs = 180 # Default 3 min
+GA511_cctv_url = "https://ga.cdn.iteris-atis.com/geojson/icons/metadata/icons.cctv.geojson"
