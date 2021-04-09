db5af94078806830579f6d8f9c31ee69d302ce66
Clay Wilson <42412332+Lwilson67gatech@users.noreply.github.com>
2021-04-09 23:30:45 +0100

    OpenCV adjustments to work on the raspberry pi

---
+++ BasicFileManagement.py
@@ -0,0 +1,16 @@
+import os
+from datetime import date
+import OpenCVTEst
+
+OpenCVTEst.recordCamera(r'/home/pi/Downloads/LinuxTest',r'rtsp://vss15od.dot.ga.gov:80/cl/gdot-cam-i-75-336.stream',r'http://navigator-c2c.dot.ga.gov/snapshots/ATL-CAM-908.jpg', 20)
+
+path = os.getcwd()
+print ("The current working directory is %s" % path)
+finalpath = path + "\TestDirectory-" + date.today().strftime("%B %d, %Y")
+
+try:
+    os.mkdir(finalpath)
+except OSError:
+    print ("Creation of the directory %s failed" % path)
+else:
+    print ("Successfully created the directory %s " % path)
---
+++ OpenCVTEst.py
@@ -0,0 +1,89 @@
+import numpy as np
+import cv2
+import os
+import requests
+from datetime import date
+from datetime import time
+from datetime import timedelta
+from datetime import datetime
+
+
+def recordCamera(directory, streamLink, imageLink, duration):
+    # Open a sample video available in sample-videos
+    stream = streamLink
+    url = imageLink
+    camera = stream[stream.rfind("/"):stream.rfind(".")].replace('/', '')
+    # print(stream.rfind("/"))
+    # print(stream.rfind("."))
+    # print(camera)
+    vcap = cv2.VideoCapture(stream)
+    tripNum = 2
+
+    print(str(datetime.now()))
+
+    path = directory
+    print ("The current working directory is %s" % path)
+    firstpath = path
+
+    try:
+        os.mkdir(firstpath)
+    except OSError:
+        print ("Creation of the directory %s failed" % firstpath)
+    else:
+        print ("Successfully created the directory %s " % firstpath)
+
+    finalpath = firstpath + "/TestDirectory-" + camera + "-" + date.today().strftime("%B %d, %Y")
+
+    filename = finalpath + "/" + datetime.now().strftime("%Y%m%d-%H%M%S") + ".avi"
+    print(filename)
+
+    try:
+        os.mkdir(finalpath)
+    except OSError:
+        print ("Creation of the directory %s failed" % finalpath)
+    else:
+        print ("Successfully created the directory %s " % finalpath)
+
+    if not vcap.isOpened():
+        print("File Cannot be Opened")
+        r = requests.get(url)
+        filenamePicture = finalpath + "/" + datetime.now().strftime("%Y%m%d-%H%M%S") + r".jpg"
+        print(filenamePicture)
+        with open(filenamePicture, "wb") as f:
+            f.write(r.content)
+    else:
+        frame_width = int(vcap.get(3))
+        frame_height = int(vcap.get(4))
+        size = (frame_width, frame_height)
+
+        video = cv2.VideoWriter(filename, cv2.VideoWriter_fourcc(*'MJPG'), 10, size)
+
+        # try:
+        #     os.mkdir(finalpath)
+        # except OSError:
+        #     print ("Creation of the directory %s failed" % finalpath)
+        # else:
+        #     print ("Successfully created the directory %s " % finalpath)
+        endTime = datetime.now() + timedelta(seconds=duration)
+        while(True):
+            # Capture frame-by-frame
+            ret, frame = vcap.read()
+            #print cap.isOpened(), ret
+            if frame is not None:
+                # Display the resulting frame
+                # cv2.imshow('frame',frame)
+                video.write(frame)
+                # Press q to close the video windows before it ends if you want
+                if cv2.waitKey(22) & 0xFF == ord('q'):
+                    break
+                if datetime.now() >= endTime:
+                    break
+            else:
+                print("is None")
+                break
+
+        # When everything done, release the capture
+        vcap.release()
+        video.release()
+        cv2.destroyAllWindows()
+        print("Video stop")
