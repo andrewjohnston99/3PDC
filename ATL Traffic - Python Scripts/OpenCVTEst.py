2ca000873e2359ffc31baab31b241aaf4dc31abd
Clay Wilson <lwilson67@mail.gatech.edu>
2021-04-08 23:31:24 -0400

    OpenCV generates a session folder; It can now grab the image if the livestream is unavailable; I have converted it to a function

--- ATL Traffic - Python Scripts/OpenCVTEst.py
+++ ATL Traffic - Python Scripts/OpenCVTEst.py
@@ -1,61 +1,86 @@
 import numpy as np
 import cv2
 import os
+import requests
 from datetime import date
 from datetime import time
 from datetime import datetime
 
-# Open a sample video available in sample-videos
-stream = 'rtsp://vss15od.dot.ga.gov:80/cl/gdot-cam-i-75-336.stream'
-camera = stream[stream.rfind("/"):stream.rfind(".")].replace('/', '')
-# print(stream.rfind("/"))
-# print(stream.rfind("."))
-# print(camera)
-vcap = cv2.VideoCapture(stream)
 
-print(str(datetime.now()))
+def recordCamera(directory,streamLink, imageLink):
+    # Open a sample video available in sample-videos
+    stream = 'rtsp://vss15od.dot.ga.gov:80/cl/gdot-cam-i-75-336.stream'
+    url = 'http://navigator-c2c.dot.ga.gov/snapshots/ATL-CAM-908.jpg'
+    camera = stream[stream.rfind("/"):stream.rfind(".")].replace('/', '')
+    # print(stream.rfind("/"))
+    # print(stream.rfind("."))
+    # print(camera)
+    vcap = cv2.VideoCapture(stream)
+    tripNum = 2
 
-if not vcap.isOpened():
-    print("File Cannot be Opened")
+    print(str(datetime.now()))
 
-frame_width = int(vcap.get(3))
-frame_height = int(vcap.get(4))
-size = (frame_width, frame_height)
+    path = os.getcwd()
+    print ("The current working directory is %s" % path)
+    firstpath = path + "\Trip-" + str(tripNum)
 
-path = os.getcwd()
-print ("The current working directory is %s" % path)
-finalpath = path + "\TestDirectory-" + camera + "-" + date.today().strftime("%B %d, %Y")
-
-filename = finalpath + chr(92) + datetime.now().strftime("%Y%m%d-%H%M%S") + ".avi"
-print(filename)
-
-video = cv2.VideoWriter(filename, cv2.VideoWriter_fourcc(*'MJPG'), 10, size)
-
-
-try:
-    os.mkdir(finalpath)
-except OSError:
-    print ("Creation of the directory %s failed" % path)
-else:
-    print ("Successfully created the directory %s " % path)
-
-while(True):
-    # Capture frame-by-frame
-    ret, frame = vcap.read()
-    #print cap.isOpened(), ret
-    if frame is not None:
-        # Display the resulting frame
-        cv2.imshow('frame',frame)
-        video.write(frame)
-        # Press q to close the video windows before it ends if you want
-        if cv2.waitKey(22) & 0xFF == ord('q'):
-            break
+    try:
+        os.mkdir(firstpath)
+    except OSError:
+        print ("Creation of the directory %s failed" % firstpath)
     else:
-        print("is None")
-        break
+        print ("Successfully created the directory %s " % firstpath)
 
-# When everything done, release the capture
-vcap.release()
-video.release()
-cv2.destroyAllWindows()
-print("Video stop")
+    finalpath = firstpath + "\TestDirectory-" + camera + "-" + date.today().strftime("%B %d, %Y")
+
+    filename = finalpath + chr(92) + datetime.now().strftime("%Y%m%d-%H%M%S") + ".avi"
+    print(filename)
+
+    try:
+        os.mkdir(finalpath)
+    except OSError:
+        print ("Creation of the directory %s failed" % finalpath)
+    else:
+        print ("Successfully created the directory %s " % finalpath)
+
+    if not vcap.isOpened():
+        print("File Cannot be Opened")
+        r = requests.get(url)
+        filenamePicture = finalpath + chr(92) + datetime.now().strftime("%Y%m%d-%H%M%S") + r".jpg"
+        print(filenamePicture)
+        with open(filenamePicture, "wb") as f:
+            f.write(r.content)
+    else:
+        frame_width = int(vcap.get(3))
+        frame_height = int(vcap.get(4))
+        size = (frame_width, frame_height)
+
+        video = cv2.VideoWriter(filename, cv2.VideoWriter_fourcc(*'MJPG'), 10, size)
+
+        # try:
+        #     os.mkdir(finalpath)
+        # except OSError:
+        #     print ("Creation of the directory %s failed" % finalpath)
+        # else:
+        #     print ("Successfully created the directory %s " % finalpath)
+
+        while(True):
+            # Capture frame-by-frame
+            ret, frame = vcap.read()
+            #print cap.isOpened(), ret
+            if frame is not None:
+                # Display the resulting frame
+                cv2.imshow('frame',frame)
+                video.write(frame)
+                # Press q to close the video windows before it ends if you want
+                if cv2.waitKey(22) & 0xFF == ord('q'):
+                    break
+            else:
+                print("is None")
+                break
+
+        # When everything done, release the capture
+        vcap.release()
+        video.release()
+        cv2.destroyAllWindows()
+        print("Video stop")
