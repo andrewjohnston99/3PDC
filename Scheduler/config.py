18c1c90f52b8cbda8683779e9f0aeda903991a3e
Andrew Johnston <42980380+andrewjohnston99@users.noreply.github.com>
2021-04-10 12:33:42 -0400

    Update config.py

--- Scheduler/config.py
+++ Scheduler/config.py
@@ -1,8 +1,9 @@
-# Config File for ATLTrafficLog Scheduler
+# Config File for 3PDC Scheduler
 
+platform = "LINUX" # Replace with "WINDOWS" for Development on Windows, defaut Linux for Pi
 cookie_file = "google.com_cookies.txt"
 rpi_google_email = "YOUR_PI_EMAIL_HERE"
 person_full_name = "YOUR FULL NAME HERE (From Google Account that will initiate the share)"
-cfb_polling_interval_secs = 180 # Default 3 min
+cfb_polling_interval_secs = 60 # Default 1 min
 GA511_cctv_url = "https://ga.cdn.iteris-atis.com/geojson/icons/metadata/icons.cctv.geojson"
 GA511_local_geojson = "icons.cctv.geojson"
