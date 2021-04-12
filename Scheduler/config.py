cb6caed10afae9f2f1a552c616ff9dba1bc4d1b9
Andrew Johnston <42980380+andrewjohnston99@users.noreply.github.com>
2021-04-12 12:50:04 -0400

    Update config.py

--- Scheduler/config.py
+++ Scheduler/config.py
@@ -4,6 +4,7 @@ platform = "LINUX" # Replace with "WINDOWS" for Development on Windows, defaut L
 cookie_file = "google.com_cookies.txt"
 rpi_google_email = "YOUR_PI_EMAIL_HERE"
 person_full_name = "YOUR FULL NAME HERE (From Google Account that will initiate the share)"
+max_cams = 10 # Only change if using system more powerful than Pi, may break for large numbers
 cfb_polling_interval_secs = 60 # Default 1 min
 GA511_cctv_url = "https://ga.cdn.iteris-atis.com/geojson/icons/metadata/icons.cctv.geojson"
 GA511_local_geojson = "icons.cctv.geojson"
