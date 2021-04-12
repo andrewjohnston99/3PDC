ae2b5f553e14d64ed3b988216ce75013c99c61aa
Clay Wilson <lwilson67@mail.gatech.edu>
2021-04-12 13:57:34 -0400

    Added cleanup script

---
+++ Scheduler/cleanupRecordings.py
@@ -0,0 +1,21 @@
+import shutil
+import datetime
+
+# Target directory takes in the base directory containing folders labelled by date
+# DaysPast is how many days ago do you want to delete the files from (0 = Today, 1 = Yesterday, etc)
+def cleanDirectory (targetDirectory, daysPast)
+
+    today = datetime.datetime.today()
+    cutoff = datetime.timedelta(days=daysPast)
+    targetDate = today - cutoff
+    targetDateString = targetDate.strftime("%B %d, %Y")
+
+    directory = targetDirectory
+    for filename in os.listdir(directory):
+        if filename.find(targetDateString) > -1:
+            try:
+                shutil.rmtree(os.path.join(directory, filename))
+            except OSError as e:
+                print("Error: %s : %s" % (dir_path, e.strerror))
+        else:
+            continue
