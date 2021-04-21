04161fc2282c1c0f737b07ab5bf9dc562de10b77
Clay Wilson <lwilson67@mail.gatech.edu>
2021-04-21 12:12:12 -0400

    Better Documentation on Clay's scripts

--- Scheduler/cleanupRecordings.py
+++ Scheduler/cleanupRecordings.py
@@ -4,12 +4,13 @@ import datetime
 # Target directory takes in the base directory containing folders labelled by date
 # DaysPast is how many days ago do you want to delete the files from (0 = Today, 1 = Yesterday, etc)
 def cleanDirectory (targetDirectory, daysPast):
-
+    #First, determine the exact date of the daysPast
     today = datetime.datetime.today()
     cutoff = datetime.timedelta(days=daysPast)
     targetDate = today - cutoff
     targetDateString = targetDate.strftime("%B %d, %Y")
 
+    #Delete the entire directory
     directory = targetDirectory
     for filename in os.listdir(directory):
         if filename.find(targetDateString) > -1:
